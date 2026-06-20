953c2201aba7d5805880616b2965ea3ff550261b
Felipe Andreas Silva <134702945+FelipeAndreasSilvaa@users.noreply.github.com>
2026-06-20 11:15:24 -0300

    feat: Implementação de users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { WorkOrder } from './work-orders/entities/work-order.entity';
 import { HealthModule } from './health/health.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 
 
